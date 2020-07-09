f9808133c16a0fd1a18b5689bfc7b354458a8a90
kaulc <46368366+kaulc@users.noreply.github.com>
2020-07-09 18:40:48 -0500

    Fix navbar sticky issue

--- jsfile.js
+++ jsfile.js
@@ -11,11 +11,12 @@ var sticky = navbar.offsetTop;
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
-  if (window.pageYOffset >= sticky) {
+  if (window.pageYOffset >= sticky * 3) {
     navbar.classList.add("sticky")
+ 
   } else {
     navbar.classList.remove("sticky");
-   
+  
   }
 }
 
